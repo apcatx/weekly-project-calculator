f6441c1276784bd980fa4caf86939e99591abcd5
Austin Carey <austinpatrickcarey@gmail.com>
2017-07-22 13:22:59 -0400

    final with single expressioncapability

--- main.js
+++ main.js
@@ -1,9 +1,56 @@
-function c(val){
-  document.getElementById('display').value = val;
-}
-function math(val){
-  document.getElementById('display').value += val;
-}
-function e(){
-  c(eval(document.getElementById('display').value));
-}
+let add = document.getElementById('add');
+let subtract = document.getElementById('subtract');
+let multiply = document.getElementById('multiply');
+let divide = document.getElementById('divide');
+let clear = document.getElementById('clear');
+let equals = document.getElementById('equals');
+
+let evaluateArray = []; 
+
+let num = document.querySelectorAll('.num');
+
+for (i = 0; i < num.length; i++) {
+  num[i].addEventListener('click', function(stuff){
+    let trueNumValue = parseInt(stuff.target.value);
+    evaluateArray.push(trueNumValue);
+    document.getElementById('display').value += stuff.target.value;
+  })
+};
+
+add.addEventListener('click', function(stuff) {
+  document.getElementById('display').value += '+';
+
+  evaluateArray.push('+');
+});
+subtract.addEventListener('click', function(stuff) {
+  document.getElementById('display').value += '-';
+  evaluateArray.push('-');
+});
+multiply.addEventListener('click', function(stuff) {
+  document.getElementById('display').value += '*';
+  evaluateArray.push('*');
+});
+divide.addEventListener('click', function(stuff) {
+  document.getElementById('display').value += '/';
+  evaluateArray.push('/');
+});
+
+
+equals.addEventListener('click', function(stuff) {
+  var a = evaluateArray[0];
+  var b = evaluateArray[2];
+  if (evaluateArray[1] === '+') {
+    var results = a + b;
+  } else if (evaluateArray[1] === '-') {
+    var results = a - b;
+  } else if (evaluateArray[1] === '/') {
+    var results = a / b;
+  } else if (evaluateArray[1] === '*') {
+    var results = a * b;
+  }
+  document.getElementById('display').value = results;
+});
+
+clear.addEventListener('click', function(stuff) {
+  document.getElementById('display').value = '';
+});
